1df6ceee0633c855644b416533eaf2a63fd85b55
Zhanna Sarbassova <zhsa@tengizchevroil.com>
2021-11-02 18:31:27 +0500

    first commit

---
+++ employees-json.js
@@ -0,0 +1,29 @@
+const DATA = { 
+ 
+    employees: [ 
+       { 
+        id: 1, 
+        name: "Пафнутий", 
+        surname: "Пафнутьев", 
+        department: "IT" 
+       } ,
+       { 
+        id: 2, 
+        name: "Иван", 
+        surname: "Иванов", 
+        department: "IT" 
+       } ,
+       { 
+        id: 3, 
+        name: "Петр", 
+        surname: "Петров", 
+        department: "SCM" 
+       } ,
+       { 
+        id: 4, 
+        name: "Сидор", 
+        surname: "Сидоров", 
+        department: "SCM" 
+       } 
+       ] 
+    } 
